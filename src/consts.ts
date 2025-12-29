525f7e23a97ffe0b4551ac37bb67898c1a42e9bc
kagiura <58155530+kagiura@users.noreply.github.com>
2025-12-29 14:22:23 +0700

    feat: ✨ init

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "NUS Bus, MRT & Walk Club";
+export const SITE_DESCRIPTION =
+  "Interest group for students in NUS who love urban planning and all things transport! 🚌🚆🚶";
